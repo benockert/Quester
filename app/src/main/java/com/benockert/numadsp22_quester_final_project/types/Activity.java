ef25b98cae11638c939ef7f9f78bbc87ff6a6f87
Jada Letts <45326648+JadaDLetts@users.noreply.github.com>
2022-04-03 15:43:18 -0400

    using activity class instead of pastQuestActivityCard

--- app/src/main/java/com/benockert/numadsp22_quester_final_project/types/Activity.java
+++ app/src/main/java/com/benockert/numadsp22_quester_final_project/types/Activity.java
@@ -10,6 +10,17 @@ public class Activity {
     public int gPriceLevel;
     public String uQuery;
 
+    public Activity() {
+
+    }
+
+    public Activity(String gName, int gPriceLevel,
+                    String gPhotoReference) {
+        this.gName = gName;
+        this.gPriceLevel = gPriceLevel;
+        this.gPhotoReference = gPhotoReference;
+    }
+
     public Activity(String gFormattedAddress, String gName, String gPhotoReference, String gPlaceId, float gPlaceLat, float gPlaceLng, int gPriceLevel, String uQuery) {
         this.gFormattedAddress = gFormattedAddress;
         this.gName = gName;
@@ -21,14 +32,29 @@ public class Activity {
         this.uQuery = uQuery;
     }
 
+    /**
+     * getter for the address of the location
+     *
+     * @return String representing the address of the location
+     */
     public String getgFormattedAddress() {
         return gFormattedAddress;
     }
 
+    /**
+     * getter for the name of the activity location
+     *
+     * @return String representing the name of the activities location
+     */
     public String getgName() {
         return gName;
     }
 
+    /**
+     * getter for the String referencing the photo of the activity location
+     *
+     * @return String representing a reference to a photo of the activity location
+     */
     public String getgPhotoReference() {
         return gPhotoReference;
     }
@@ -45,6 +71,11 @@ public class Activity {
         return gPlaceLng;
     }
 
+    /**
+     * getter for the price range of the activity
+     *
+     * @return String representing the number of $signs the price range is
+     */
     public int getgPriceLevel() {
         return gPriceLevel;
     }
