f0ce5c00fd512df16f3e44efd7ac071df2451a0a
Nate <nateps313@gmail.com>
2022-04-12 15:26:13 -0400

    added active quest activity and view

---
+++ app/src/main/java/com/benockert/numadsp22_quester_final_project/activeQuest/ActiveQuest.java
@@ -0,0 +1,27 @@
+package com.benockert.numadsp22_quester_final_project.activeQuest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.benockert.numadsp22_quester_final_project.R;
+
+public class ActiveQuest extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_quest);
+    }
+
+    public void goToNextStop(View view) {
+    }
+
+    public void goToCurrentStop(View view) {
+
+    }
+}
