16547408afe64263cc1e74db0fe1b9248685c0a2
Jada <jletts77@gmail.com>
2022-04-18 02:11:03 -0400

    updated comments and variable names, added recapCardListener

--- app/src/main/java/com/benockert/numadsp22_quester_final_project/PhotoRecap/ViewAllRecapsAdapter.java
+++ app/src/main/java/com/benockert/numadsp22_quester_final_project/PhotoRecap/ViewAllRecapsAdapter.java
@@ -13,9 +13,19 @@ import java.util.ArrayList;
 
 public class ViewAllRecapsAdapter extends RecyclerView.Adapter<ViewAllRecapsHolder> {
     private final ArrayList<RecapCard> rCardList;
+    private RecapCardListener rc_listener;
 
     public ViewAllRecapsAdapter(ArrayList<RecapCard> rCardList) {
         this.rCardList = rCardList;
+    }
+
+    /**
+     * sets the recap card listener of the current adapter
+     *
+     * @param rc_listener the given link listener the adapter will soon have
+     */
+    public void setRc_listener(RecapCardListener rc_listener){
+        this.rc_listener = rc_listener;
     }
 
     /**
@@ -29,22 +39,22 @@ public class ViewAllRecapsAdapter extends RecyclerView.Adapter<ViewAllRecapsHold
     @Override
     public ViewAllRecapsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recap_card, parent, false);
-        return new ViewAllRecapsHolder(view);
+        return new ViewAllRecapsHolder(view, rc_listener);
     }
 
     /**
      * Called by RecyclerView to display data at a specified position
      *
      * @param holder   ViewAllRecapsHolder
-     * @param position Integer
+     * @param pos Integer representing the position of the current recap card
      */
     @Override
-    public void onBindViewHolder(@NonNull ViewAllRecapsHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewAllRecapsHolder holder, int pos) {
         //getting the current card
-        RecapCard currentCard = rCardList.get(position);
+        RecapCard currentCard = rCardList.get(pos);
         //setting the values of the current card
-        holder.recapName.setText(currentCard.getRecapName());
-        holder.recapDate.setText(currentCard.getRecapDate());
+        holder.recapName.setText(currentCard.getRecapName(pos));
+        holder.recapDate.setText(currentCard.getRecapDate(pos));
     }
 
     /**
