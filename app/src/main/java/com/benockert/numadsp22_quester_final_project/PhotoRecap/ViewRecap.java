99257f24753180c242994d6ae757a578ee16c85f
Jada <jletts77@gmail.com>
2022-04-18 18:35:43 -0400

    basic layout to being working on templates and selecting photos
    TODO: display different templates, store recap in database, and display generated recap

--- app/src/main/java/com/benockert/numadsp22_quester_final_project/PhotoRecap/ViewRecap.java
+++ app/src/main/java/com/benockert/numadsp22_quester_final_project/PhotoRecap/ViewRecap.java
@@ -3,14 +3,26 @@ package com.benockert.numadsp22_quester_final_project.PhotoRecap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.benockert.numadsp22_quester_final_project.R;
 
 public class ViewRecap extends AppCompatActivity {
-
+    String questRecapName;
+    TextView recapName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recap);
+        questRecapName = this.getIntent().getExtras().getString("recapName");
+        recapName = findViewById(R.id.viewRecapRName);
+        recapName.setText(questRecapName);
+        Log.i("inViewRecap", questRecapName);
+    }
+
+    public void backToViewAll(View v){
+        finish();
     }
 }
