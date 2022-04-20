6637f5104f7f29cf6a79256aef7999441cf2efc8
Jada <jletts77@gmail.com>
2022-04-20 17:52:14 -0400

    adds information to firebase db

--- app/src/main/java/com/benockert/numadsp22_quester_final_project/Templates/ChooseTemplate.java
+++ app/src/main/java/com/benockert/numadsp22_quester_final_project/Templates/ChooseTemplate.java
@@ -13,6 +13,13 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.benockert.numadsp22_quester_final_project.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class ChooseTemplate extends AppCompatActivity {
     String chosenTemplate;
@@ -96,6 +103,18 @@ public class ChooseTemplate extends AppCompatActivity {
     }
 
     public void choosePhotos(View v) {
+        DatabaseReference dr = FirebaseDatabase.getInstance().getReference();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String userName = Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName();
+        assert userName != null;
+        String dateGenerated = String.valueOf(LocalDate.now());
+
+        dr.child("users").child(userName).child("recaps")
+                .child(recapName).child("template").setValue(chosenTemplate);
+
+        dr.child("users").child(userName).child("recaps")
+                .child(recapName).child("dateGenerated").setValue(dateGenerated);
+
         Intent i = new Intent(this, ChoosePhotos.class);
         i.putExtra("chosenTemplateName", chosenTemplate);
         i.putExtra("recapName", recapName);
