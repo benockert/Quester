525eeabbd9c2b4b463d68c17d7151fb29868d702
Jada <jletts77@gmail.com>
2022-04-21 12:19:23 -0400

    attempt at view recap and choose photos

--- app/src/main/java/com/benockert/numadsp22_quester_final_project/PhotoRecap/ViewRecap.java
+++ app/src/main/java/com/benockert/numadsp22_quester_final_project/PhotoRecap/ViewRecap.java
@@ -1,28 +1,104 @@
 package com.benockert.numadsp22_quester_final_project.PhotoRecap;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.benockert.numadsp22_quester_final_project.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class ViewRecap extends AppCompatActivity {
     String questRecapName;
-    TextView recapName;
+    FirebaseStorage storage;
+    String username;
+    DatabaseReference dr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recap);
         questRecapName = this.getIntent().getExtras().getString("recapName");
-        recapName = findViewById(R.id.viewRecapRName);
-        recapName.setText(questRecapName);
-        Log.i("inViewRecap", questRecapName);
+        storage = FirebaseStorage.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        dr = FirebaseDatabase.getInstance().getReference();
+
+        username = Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName();
+        Toolbar toolbar = findViewById(R.id.viewRecapToolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_share_24);
+
+        TextView title = findViewById(R.id.toolbar_title);
+        title.setText(questRecapName);
+
+        toolbar.setNavigationOnClickListener(view -> {
+            Log.i("success", "success");
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            // shareIntent.putExtra(Intent.EXTRA_STREAM, uriToImage);
+            shareIntent.setType("image/jpg");
+            startActivity(Intent.createChooser(shareIntent, null));
+        });
+
+        retrieveRecap();
     }
 
-    public void backToViewAll(View v){
-        finish();
+    private void retrieveRecap() {
+        //        retrieves and displays recap that was generated or saved
+
+        // ImageView in your Activity
+        ImageView imageView = findViewById(R.id.viewRecapImg);
+        // Reference to an image file in Cloud Storage
+        StorageReference storageRef = storage.getReference();
+        String imgRecapName = questRecapName.replaceAll("\\|", "_");
+        String path = username + "/" + imgRecapName + ".jpg";
+        StorageReference pathReference = storageRef.child(path);
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        pathReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(bytes -> {
+
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            imageView.setImageBitmap(bitmap);
+
+        }).addOnFailureListener(exception -> {
+            // Handle any errors
+        });
+    }
+
+    public void delete(View v) {
+
+      //  DatabaseReference df = dr.re
+
+        dr.child("users").child(username).child("recaps").child(questRecapName).removeValue();
+
+        Log.i("dtae", String.valueOf(LocalDate.now()));
+
+        Intent i = new Intent(this, ViewAllRecaps.class);
+        startActivity(i);
+    }
+
+    /**
+     * converts the String to a parsable json string
+     *
+     * @param s String of information gotten from the get request
+     * @return String of json info to parse through
+     */
+    private String convertStingToJson(String s) {
+        Scanner sc = new Scanner(s).useDelimiter("\\A");
+        return sc.hasNext() ? sc.next().replace(", ", ",\n") : "";
     }
 }
--- app/src/main/java/com/benockert/numadsp22_quester_final_project/Templates/ChoosePhotos.java
+++ app/src/main/java/com/benockert/numadsp22_quester_final_project/Templates/ChoosePhotos.java
@@ -1,16 +1,132 @@
 package com.benockert.numadsp22_quester_final_project.Templates;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.benockert.numadsp22_quester_final_project.PhotoRecap.ViewRecap;
 import com.benockert.numadsp22_quester_final_project.R;
 
 public class ChoosePhotos extends AppCompatActivity {
+    String recapName;
+    String template;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_photos);
+        recapName = this.getIntent().getStringExtra("recapName");
+        template = this.getIntent().getStringExtra("chosenTemplateName");
+
+        switch (template) {
+            case "Triangle 1":
+                setContentView(R.layout.triangle1_choose);
+                break;
+            case "Triangle 2":
+                setContentView(R.layout.triangle2_choose);
+                break;
+            case "Triangle 3":
+                setContentView(R.layout.triangle3_choose);
+                break;
+            case "Triangle 4":
+                setContentView(R.layout.triangle4_choose);
+                break;
+        }
+
+//        // create an instance of the
+//        // intent of the type image
+//        Intent i = new Intent();
+//        i.setType("image/*");
+//        i.setAction(Intent.ACTION_GET_CONTENT);
+//
+//        // pass the constant to compare it
+//        // with the returned requestCode
+//        startActivityForResult(Intent.createChooser(i, "Select Picture"), 5);
+    }
+
+    public void SelectImg1(View v) {
+
+        registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        assert data != null;
+                        Uri uri = data.getData();
+                        ImageView imageView = findViewById(R.id.imageView);
+                        imageView.setImageURI(uri);
+                    }
+                });
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.setType("image/*");
+    }
+
+    public void SelectImg2(View v) {
+
+    }
+
+    public void SelectImg3(View v) {
+
+    }
+
+//        // Create a storage reference from our app
+//        StorageReference storageRef = storage.getReference();
+//
+//// Create a reference to "mountains.jpg"
+//        StorageReference mountainsRef = storageRef.child("mountains.jpg");
+//
+//// Create a reference to 'images/mountains.jpg'
+//        StorageReference mountainImagesRef = storageRef.child("images/mountains.jpg");
+//
+//// While the file names are the same, the references point to different files
+//        mountainsRef.getName().equals(mountainImagesRef.getName());    // true
+//        mountainsRef.getPath().equals(mountainImagesRef.getPath());
+
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == PICK_PHOTO_FOR_AVATAR && resultCode == Activity.RESULT_OK) {
+//            if (data == null) {
+//                //Display an error
+//                return;
+//            }
+//            InputStream inputStream = context.getContentResolver().openInputStream(data.getData());
+//            //Now you can do whatever you want with your input stream, save it as file, upload to a server, decode a bitmap...
+//        }
+//
+//        if (template.equals("grid 1") || template.equals("grid 2") || template.equals("triangle")) {
+//
+//        }
+//    }
+
+    public ImageView retrieveImage() {
+        return null;
+    }
+
+    public void saveToFirebase() {
+
+    }
+
+    public void finish(View v) {
+        //TODO: save recap and image in database
+        saveToFirebase();
+
+        Intent i = new Intent(this, ViewRecap.class);
+        i.putExtra("recapName", recapName);
+        startActivity(i);
     }
 }
+
