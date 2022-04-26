4572890aae75d8f2982041e6b0cc3aa86b186a7c
Jada <jletts77@gmail.com>
2022-04-26 16:07:13 -0400

    cleaned up code, added comments

--- app/src/main/java/com/benockert/numadsp22_quester_final_project/PhotoRecap/ViewRecap.java
+++ app/src/main/java/com/benockert/numadsp22_quester_final_project/PhotoRecap/ViewRecap.java
@@ -41,12 +41,12 @@ public class ViewRecap extends AppCompatActivity {
         questRecapName = this.getIntent().getExtras().getString("recapName");
         username = Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName();
 
+        //sets the title of the recap
         title = findViewById(R.id.toolbar_title);
         title.setText(questRecapName);
 
         // Reference to an image file in Cloud Storage
         String recapName = questRecapName.replaceAll("\\|", "_");
-        Log.i("name", recapName);
         StorageReference storageReference = FirebaseStorage.getInstance().getReference();
         String path = username + "/" + recapName + ".JPEG";
         StorageReference pathReference = storageReference.child(path);
@@ -62,6 +62,11 @@ public class ViewRecap extends AppCompatActivity {
         });
     }
 
+    /**
+     * sets the on click dunction of the share button located in the toolbar
+     *
+     * @param bm BitMap representing the image to share
+     */
     private void setToolbar(Bitmap bm) {
         toolbar = findViewById(R.id.viewRecapToolbar);
         toolbar.setNavigationIcon(R.drawable.ic_baseline_share_24);
@@ -69,15 +74,15 @@ public class ViewRecap extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(view -> {
             try {
                 File cachePath = new File(this.getCacheDir(), "images");
-                //cachePath.mkdirs(); // don't forget to make the directory
-                FileOutputStream stream = new FileOutputStream(new File(cachePath, "image.png")); // overwrites this image every time
+                // overwrites this image every time
+                FileOutputStream stream = new FileOutputStream(new File(cachePath, "image.JPEG"));
                 bm.compress(Bitmap.CompressFormat.JPEG, 100, stream);
                 stream.close();
             } catch (Exception e) {
                 Log.i("error", e.getMessage());
             }
             File imagePath = new File(this.getCacheDir(), "images");
-            File newFile = new File(imagePath, "image.png");
+            File newFile = new File(imagePath, "image.JPEG");
             Uri contentUri =
                     FileProvider.getUriForFile(this,
                             "com.benockert.numadsp22_quester_final_project" +
@@ -87,47 +92,32 @@ public class ViewRecap extends AppCompatActivity {
                 Intent shareIntent = new Intent();
                 shareIntent.setAction(Intent.ACTION_SEND);
                 shareIntent.setType("image/*");
-                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // temp permission for receiving app to read this file
+                // temp permission for receiving app to read this file
+                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 shareIntent.setDataAndType(contentUri, getContentResolver().getType(contentUri));
                 shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
                 startActivity(Intent.createChooser(shareIntent, "Save Or Share Image"));
             }
-            //this code auto saves image to gallery/google photos
-            /*
- try {
- Log.i("success", "success");
- Intent shareIntent = new Intent();
-
- shareIntent.setAction(Intent.ACTION_SEND);
- shareIntent.setType("image/*");
-
- ByteArrayOutputStream b = new ByteArrayOutputStream();
- bm.compress(Bitmap.CompressFormat.JPEG, 100, b);
-
- File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), questRecapName);
- FileOutputStream stream = new FileOutputStream(file);
- bm.compress(Bitmap.CompressFormat.PNG, 90, stream);
- stream.close();
- Uri uri = Uri.fromFile(file);
- Log.i("success", uri.toString());
- // String path = MediaStore.Images.Media.insertImage(this.getContentResolver(), bm, null, null);
- //   Uri uri = Uri.parse(path);
- shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
- startActivity(Intent.createChooser(shareIntent, null));
- // });
- } catch (Exception e) {
-
- }*/
         });
     }
 
+    /**
+     * on click allow user to delete their recap and re-directs them to the view all recaps page
+     *
+     * @param v View current view
+     */
     public void delete(View v) {
         dr.child("users").child(username).child("recaps").child(questRecapName).removeValue();
         Intent i = new Intent(this, ViewAllRecaps.class);
         startActivity(i);
     }
 
-    public void toViewAllRecaps(View v){
+    /**
+     * on click method to navigate the user to the view all recaps page
+     *
+     * @param v View current view
+     */
+    public void toViewAllRecaps(View v) {
         Intent i = new Intent(this, ViewAllRecaps.class);
         startActivity(i);
     }
